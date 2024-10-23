33a759af69d600438475771c32df52c51ce65b90
lisnasitinrmlh <158829617+lisnasitinrmlh@users.noreply.github.com>
2024-10-23 07:38:58 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <style type="text/css">
+    p {
+      background: white;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <footer class="">
+      <div class="">
+        <div class="">
+          <div class="">
+            <br><br><br>
+            <p class=""><i>© 2024 Dibuat oleh Lisna Siti Nurmilah | Universitas Wanita Internasional</i></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+</body>
+</html>
